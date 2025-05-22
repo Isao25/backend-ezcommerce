dc05a63314129f15eae81d8e0978d2837bc10469
GiacomoMadrid <gsmr.18@gmail.com>
2025-05-21 21:29:55 -0500

    seeder

--- ezcommerce/settings.py
+++ ezcommerce/settings.py
@@ -83,14 +83,25 @@ WSGI_APPLICATION = 'ezcommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
+#Database Postgresql
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ezcommercedb',
+        'USER': 'postgres',
+        'PASSWORD': 'virtualmiau16',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
---
+++ scripts/seed_data.py
@@ -0,0 +1,40 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ezcommerce.settings')
+django.setup()
+
+from epica2.models import Facultad, EscuelaProfesional
+
+# Limpia datos existentes (opcional)
+Facultad.objects.all().delete()
+EscuelaProfesional.objects.all().delete()
+
+# Crear Facultades
+facultad1 = Facultad.objects.create(codigo="01", nombre="Facultad de Medicina - San Fernando", siglas="FMSF") #
+facultad2 = Facultad.objects.create(codigo="02", nombre="Facultad de Derecho y Ciencias Políticas", siglas="FDCP") # 
+facultad3 = Facultad.objects.create(codigo="03", nombre="Facultad de Letras y Ciencias Humanas", siglas="FLCH") #
+facultad4 = Facultad.objects.create(codigo="04", nombre="Facultad de Farmacia y Bioquímica", siglas="FFB") #
+facultad5 = Facultad.objects.create(codigo="05", nombre="Facultad de Odontología", siglas="FO") #
+facultad6 = Facultad.objects.create(codigo="06", nombre="Facultad de Educación", siglas="FEDU") #
+facultad7 = Facultad.objects.create(codigo="07", nombre="Facultad de Química e Ingeniería Química", siglas="FQIQ") #
+facultad8 = Facultad.objects.create(codigo="08", nombre="Facultad de Medicina Veterinaria", siglas="FMV") #
+facultad9 = Facultad.objects.create(codigo="09", nombre="Facultad de Ciencias Administrativas", siglas="FCA") #
+facultad10 = Facultad.objects.create(codigo="10", nombre="Facultad de Ciencias Biológicas", siglas="FCB") #
+facultad11 = Facultad.objects.create(codigo="11", nombre="Facultad de Ciencias Contables", siglas="FCC") #
+facultad12 = Facultad.objects.create(codigo="12", nombre="Facultad de Ciencias Económicas", siglas="FCE") #
+facultad13 = Facultad.objects.create(codigo="13", nombre="Facultad de Ciencias Físicas", siglas="FCF") #
+facultad14 = Facultad.objects.create(codigo="14", nombre="Facultad de Ciencias Matemáticas", siglas="FCM") #
+facultad15 = Facultad.objects.create(codigo="15", nombre="Facultad de Ciencias Sociales", siglas="FCS") #
+facultad16 = Facultad.objects.create(codigo="16", nombre="Facultad de Ingeniería Geológica, Minera, Metalúrgica y Geográfica", siglas="FIGMMG") #
+facultad17 = Facultad.objects.create(codigo="17", nombre="Facultad de Ingeniería Inustrial", siglas="FII") #
+facultad18 = Facultad.objects.create(codigo="18", nombre="Facultad de Psicología", siglas="FPSI") #
+facultad19 = Facultad.objects.create(codigo="19", nombre="Facultad de Ingeniería Eléctrica y Electrónica", siglas="FIEE") #
+facultad20 = Facultad.objects.create(codigo="20", nombre="Facultad de Ingeniería de Sistemas e Informática", siglas="FISI") #
+
+# Crear Escuelas Profesionales
+EscuelaProfesional.objects.create(codigo="EPIS", nombre="Ingeniería de Sistemas", id_facultad=facultad20)
+EscuelaProfesional.objects.create(codigo="EPISW", nombre="Ingeniería de Software", id_facultad=facultad20)
+EscuelaProfesional.objects.create(codigo="EPCC", nombre="Ciencias de la Computación", id_facultad=facultad20)
+
+print("Datos iniciales insertados con éxito.")
